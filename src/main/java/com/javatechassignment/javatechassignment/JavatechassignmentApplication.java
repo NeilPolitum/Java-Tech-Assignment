da244ee2cdf521cf12e72bccf613f53d2883af9a
Daniel <dsrubianop@correo.udistrital.edu.co>
2022-01-05 19:37:14 -0500

    inicial

---
+++ src/main/java/com/javatechassignment/javatechassignment/JavatechassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.javatechassignment.javatechassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatechassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatechassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechassignment/javatechassignment/JavatechassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechassignment.javatechassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatechassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
